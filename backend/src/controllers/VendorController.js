138f7183aab8a5b6c05949973cdc69c0e177f366
Md. Mustakin Ebne Mazid <mustakin.mazid@brainstation-23.com>
2026-01-29 01:13:29 +0600

    refactor: Implement login functionality for vendors with error handling

--- backend/src/controllers/VendorController.js
+++ backend/src/controllers/VendorController.js
@@ -1,4 +1,46 @@
 import Vendor from "../models/Vendor.js";
+import bcrypt from "bcrypt";
+
+export const loginVendor = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+        const vendor = await Vendor.getByEmail(email);
+        if (!vendor) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, vendor.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        res.status(200).json({
+            message: "Vendor Login successful",
+            vendor: {
+                vendor_id: vendor.vendor_id,
+                name: vendor.name,
+                email: vendor.email,
+                phone: vendor.phone,
+                shop_name: vendor.shop_name,
+                shop_type: vendor.shop_type,
+                description: vendor.description,
+                street: vendor.street,
+                area: vendor.area,
+                city: vendor.city,
+                country: vendor.country,
+                postal_code: vendor.postal_code,
+                created_at: vendor.created_at
+            }
+        });
+
+    }catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: error.message });
+    }
+};
 
 export const registerVendor = async (req, res) => {
     try {
