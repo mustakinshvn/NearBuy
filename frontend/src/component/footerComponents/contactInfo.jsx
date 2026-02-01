e4be9f419bcf64ecd052c9f101cb5b3075b089b3
Md. Mustakin Ebne Mazid <mustakin.mazid@brainstation-23.com>
2026-02-01 15:47:34 +0600

    refactor: Adjust layout for ContactInfo component to improve alignment

--- frontend/src/component/footerComponents/contactInfo.jsx
+++ frontend/src/component/footerComponents/contactInfo.jsx
@@ -1,9 +1,9 @@
 import { Mail, Phone, MapPin } from "lucide-react";
 const ContactInfo = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center ">
       <h4 className="text-lg font-bold mb-4">Contact Us</h4>
-      <ul className="space-y-3 flex  items-center  flex-wrap gap-4">
+      <ul className="space-y-3 flex  items-center justify-center  flex-wrap gap-4">
         <li className="flex items-start gap-2">
           <MapPin className="size-5 text-blue-400  " />
           <span className="text-slate-300">Dhaka, Bangladesh</span>
